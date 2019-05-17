0e6b4df1510038553379e319a4c887ada650f248
Stanley Song <luge1123@gmail.com>
2019-05-17 23:47:06 +0800

    initial commit

---
+++ Dota2/deep_versions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 10:44:26 2018
+
+@author: song
+"""
+# theano
+#import theano
+#print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
---
+++ Dota2/versions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  8 10:33:42 2018
+
+@author: song
+"""
+
+# scipy
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+import pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
